043ccc924abb84bf044bd24342a92ec252a5b52a
krazylink <paul.huckabee@gmail.com>
2014-02-19 13:19:28 -0500

    adding file upload

--- char_sheet/views.py
+++ char_sheet/views.py
@@ -12,7 +12,7 @@ from django.conf import settings
 from char_sheet.models import Characters
 from character_sheet.DND4Eparser import *
 from xml.dom import minidom
-from forms import UserForm
+from .forms import UploadFileForm
 import os 
 
 @login_required
@@ -23,60 +23,65 @@ def userView(request):
 
 @login_required
 def createCharacter(request):
-  f = open(os.path.join(settings.PROJECT_ROOT,"ImmeralLvl3.dnd4e"))
-  dom = minidom.parse(f)
-  details = handleDetails(dom.getElementsByTagName("Details")[0])
-  stats = handleStats(dom.getElementsByTagName("Stat"))
+  if request.method == 'POST':
+    form = UploadFileForm(request.POST, request.FILES)
+    if form.is_valid():
+      dom = minidom.parse(request.FILES['file'])
+      details = handleDetails(dom.getElementsByTagName("Details")[0])
+      stats = handleStats(dom.getElementsByTagName("Stat"))
 
-  character = Characters(
-      user               = User.objects.get(username=request.user),
-      name               = str(details["name"]),
-      xp                 = int(details["Experience"]),
-      level              = int(details["Level"]),
-      death_save_count   = int(stats["Death Saves Count"]),
-      healing_surges     = int(stats["Healing Surges"]),
-      hit_points         = int(stats["Hit Points"]),
-      action_points      = int(stats["_BaseActionPoints"]),
-      strength           = int(stats["Strength"]),
-      constitution       = int(stats["Constitution"]),
-      dexterity          = int(stats["Dexterity"]),
-      intelligence       = int(stats["Intelligence"]),
-      wisdom             = int(stats["Wisdom"]),
-      charisma           = int(stats["Charisma"]),
-      strength_mod       = int(stats["Strength modifier"]),
-      constitution_mod   = int(stats["Constitution modifier"]),
-      dexterity_mod      = int(stats["Dexterity modifier"]),
-      intelligence_mod   = int(stats["Intelligence modifier"]),
-      wisdom_mod         = int(stats["Wisdom modifier"]),
-      charisma_mod       = int(stats["Charisma modifier"]),
-      ac                 = int(stats["AC"]),
-      fortitude          = int(stats["Fortitude Defense"]),
-      reflex             = int(stats["Reflex Defense"]),
-      will               = int(stats["Will Defense"]),
-      acrobatics         = int(stats["Acrobatics"]),
-      arcana             = int(stats["Arcana"]),
-      athletics          = int(stats["Athletics"]),
-      bluff              = int(stats["Bluff"]),
-      diplomacy          = int(stats["Diplomacy"]),
-      dungeoneering      = int(stats["Dungeoneering"]),
-      endurance          = int(stats["Endurance"]),
-      heal               = int(stats["Heal"]),
-      history            = int(stats["History"]),
-      insight            = int(stats["Insight"]),
-      intimidate         = int(stats["Intimidate"]),
-      nature             = int(stats["Nature"]),
-      perception         = int(stats["Perception"]),
-      religion           = int(stats["Religion"]),
-      stealth            = int(stats["Stealth"]),
-      streetwise         = int(stats["Streetwise"]),
-      thievery           = int(stats["Thievery"]),
-      initiative         = int(stats["Initiative"]),
-      passive_insight    = int(stats["Passive Insight"]),
-      passive_perception = int(stats["Passive Perception"]),
-      speed              = int(stats["Speed"]),
-      )
-  character.save()
-  return HttpResponse("Character %s created." % (str(details["name"])))
+      character = Characters(
+          user               = User.objects.get(username=request.user),
+          name               = str(details["name"]),
+          xp                 = int(details["Experience"]),
+          level              = int(details["Level"]),
+          death_save_count   = int(stats["Death Saves Count"]),
+          healing_surges     = int(stats["Healing Surges"]),
+          hit_points         = int(stats["Hit Points"]),
+          action_points      = int(stats["_BaseActionPoints"]),
+          strength           = int(stats["Strength"]),
+          constitution       = int(stats["Constitution"]),
+          dexterity          = int(stats["Dexterity"]),
+          intelligence       = int(stats["Intelligence"]),
+          wisdom             = int(stats["Wisdom"]),
+          charisma           = int(stats["Charisma"]),
+          strength_mod       = int(stats["Strength modifier"]),
+          constitution_mod   = int(stats["Constitution modifier"]),
+          dexterity_mod      = int(stats["Dexterity modifier"]),
+          intelligence_mod   = int(stats["Intelligence modifier"]),
+          wisdom_mod         = int(stats["Wisdom modifier"]),
+          charisma_mod       = int(stats["Charisma modifier"]),
+          ac                 = int(stats["AC"]),
+          fortitude          = int(stats["Fortitude Defense"]),
+          reflex             = int(stats["Reflex Defense"]),
+          will               = int(stats["Will Defense"]),
+          acrobatics         = int(stats["Acrobatics"]),
+          arcana             = int(stats["Arcana"]),
+          athletics          = int(stats["Athletics"]),
+          bluff              = int(stats["Bluff"]),
+          diplomacy          = int(stats["Diplomacy"]),
+          dungeoneering      = int(stats["Dungeoneering"]),
+          endurance          = int(stats["Endurance"]),
+          heal               = int(stats["Heal"]),
+          history            = int(stats["History"]),
+          insight            = int(stats["Insight"]),
+          intimidate         = int(stats["Intimidate"]),
+          nature             = int(stats["Nature"]),
+          perception         = int(stats["Perception"]),
+          religion           = int(stats["Religion"]),
+          stealth            = int(stats["Stealth"]),
+          streetwise         = int(stats["Streetwise"]),
+          thievery           = int(stats["Thievery"]),
+          initiative         = int(stats["Initiative"]),
+          passive_insight    = int(stats["Passive Insight"]),
+          passive_perception = int(stats["Passive Perception"]),
+          speed              = int(stats["Speed"]),
+          )
+      character.save()
+      return HttpResponseRedirect('/char_sheet/')
+  else:
+    form = UploadFileForm()
+  return render(request, "char_sheet/upload.html", {"form": form})
 
 def displayCharacter(request, character_id):
   character = Characters.objects.get(pk=character_id)
