228b33b4f2f231c0aae476e2f5f9bbcbcec688fa
krazylink <paul.huckabee@gmail.com>
2014-02-19 09:51:42 -0500

    fixing urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from accounts import views
 
 urlpatterns = patterns('',
-    url(r'^login/', views.loginView, name="login"),
-    url('r/^logout/', views.logoutView, name="logout"),
+    url(r"^login/", views.loginView, name="login"),
+    url(r"^logout/", views.logoutView, name="logout"),
     url(r"^createUser/", views.newUser, name="user")
     )
 
