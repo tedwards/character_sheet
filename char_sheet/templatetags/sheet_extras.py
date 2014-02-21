63ab450d0389c5b4f746aa9e6ae29d06915ba3f0
krazylink <paul.huckabee@gmail.com>
2014-02-21 02:22:18 -0500

    added tags for bolding first word. Added power card css

--- char_sheet/templatetags/sheet_extras.py
+++ char_sheet/templatetags/sheet_extras.py
@@ -5,3 +5,14 @@ register = template.Library()
 @register.filter
 def get(mapping, key):
   return mapping.get(key, '')
+
+@register.filter
+def bold_first(value):
+  s = str(value)
+  s = s.split()
+  if s:
+    s[0] = "<b>%s</b>" % s[0]
+    s = ' '.join(s)
+    return s
+  else:
+    return ''
